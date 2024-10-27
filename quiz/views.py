9dd6d79736258328bcb1449b4398ab903da544a5
hadi499 <hadi.ygcl@gmail.com>
2024-10-27 18:54:50 +0700

    up

--- quiz/views.py
+++ quiz/views.py
@@ -28,52 +28,6 @@ def quiz_take(request, pk):
     return render(request, 'quiz/quiz_take.html', {'quiz': quiz, 'questions': questions})
 
 
-# @login_required
-# def quiz_submit(request, pk):
-#     if request.method == "POST":
-#         quiz = get_object_or_404(Quiz, pk=pk)
-#         question_ids = request.session.get('quiz_questions', [])
-#         questions = Question.objects.filter(id__in=question_ids).order_by('id')
-        
-#         score = 0
-#         correct_answers = 0
-#         question_details = []
-
-#         for question in questions:
-#             selected_answer = request.POST.get(f'question_{question.id}')
-#             is_correct = selected_answer == question.correct_answer
-            
-#             if is_correct:
-#                 correct_answers += 1
-            
-#             question_details.append({
-#                 'title': question.title,
-#                 'selected_answer': selected_answer,
-#                 'correct_answer': question.correct_answer,
-#                 'is_correct': is_correct
-#             })
-
-#         total_questions = len(questions)
-#         score = (correct_answers / total_questions) * 100
-
-#         Result.objects.create(quiz=quiz, user=request.user, score=score)
-        
-#         passed = score >= quiz.required_score_to_pass
-
-#         # Hapus data sesi setelah digunakan
-#         if 'quiz_questions' in request.session:
-#             del request.session['quiz_questions']
-
-#         return render(request, 'quiz/quiz_result.html', {
-#             'quiz': quiz,
-#             'score': score,
-#             'passed': passed,
-#             'total_questions': total_questions,
-#             'correct_answers': correct_answers,
-#             'question_details': question_details
-#         })
-#     return redirect('quiz_list')
-
 @login_required
 def scores_quiz(request):  
   user_id = request.user.id  
